204bfc8354aa44bf19ea7f94520fada095b7b724
Warwick Stone <warwick_stone@fastmail.com>
2022-01-31 20:06:36 +1100

    group agreements by party

--- netlify/functions/policy/policy.ts
+++ netlify/functions/policy/policy.ts
@@ -6,10 +6,22 @@ export const handler: Handler = async (event, context) => {
   const { id } = event.queryStringParameters;
 
   const policy = await fetch(`https://theyvoteforyou.org.au/api/v1/policies/${id}.json?key=${apiKey}`)
-    .then(response => response.json());
+    .then(response => response.json()) as any;
+    // todo: add API type
+
+  const partyAgreements = policy.people_comparisons.reduce((parties, pc) => {
+    const party = pc.person.latest_member.party;
+    if (!parties[party]) {
+      parties[party] = [];
+    }
+
+    parties[party].push(pc.agreement);
+
+    return parties;
+  }, {});
 
   return {
     statusCode: 200,
-    body: JSON.stringify(policy),
+    body: JSON.stringify(partyAgreements),
   }
 }
